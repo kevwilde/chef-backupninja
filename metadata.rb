e368f05c32a4f35afd3fdd59e881657bbfcf280c
Kevin Van Wilder <kevin@inuits.eu>
2014-09-08 09:33:15 +0200

    Empty cookbook.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'backupninja'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures backupninja'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
