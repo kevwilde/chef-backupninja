3d062d86310897f082ac31a62862bc3b30aa22f3
Kevin Van Wilder <kevin@inuits.eu>
2014-09-10 16:00:06 +0200

    Removed test code :)

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,27 +20,3 @@ template node['backupninja']['general_configfile'] do
 	source 'backupninja.conf.erb'
 	owner 'root'
 end
-
-
-backupninja_rdiff 'tmpbackup' do
-	priority 		10
-	description		'jos, tes ek ie, da macheert ie ni zenne'
-	when_executed	'hourly'
-	
-	source_keep		185
-	source_include	[
-		'/var/spool/cron/crontabs',
-		'/var/backups',
-		'/home',
-		'/usr/local/bin'
-	]
-	source_exclude	[
-		'/home/*/.gnupg',
-		'/home/*/.ssh'
-	]
-
-	dest_host		'ow-backups'
-	dest_directory	'/backups'
-
-end
-
